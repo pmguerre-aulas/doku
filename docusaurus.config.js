7f43203f53629bddeede117894939fa21ad24584
Pedro Guerreiro <pmguerre@ualg.pt>
2023-12-21 15:53:20 +0000

    create a link with html

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,8 +98,11 @@ const config = {
             ],*/
             items: [
               {
-                label: 'Ola...',
+                //label: 'Ola...',
                 //to: '/docs/intro',
+                html: '<a href="https://www.netlify.com" target="_blank" rel="noreferrer noopener" aria-label="Deploys by Netlify">
+                  <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" width="114" height="51" />
+                </a>'
               },
             ],
           },
